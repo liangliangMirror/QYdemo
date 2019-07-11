622b5ef6dedac298d4e7ed70ca523821268fb6e6
liangliangMirror <jxliangweihan@gmail.com>
2019-07-11 17:30:40 +0800

    7-11xx

--- src/views/location.vue
+++ src/views/location.vue
@@ -2,10 +2,13 @@
   <div>
     <van-nav-bar title="切换国家城市" left-text="返回" left-arrow @click-left="onClickLeft" />
     <van-search placeholder="请输入搜索关键词" v-model="value" />
+    <history-l></history-l>
   </div>
 </template>
 <script>
 import Vue from "vue";
+import historyL from "../components/location/historyl";
+import { mapState } from "vuex";
 import { NavBar } from "vant";
 import { Search } from "vant";
 
@@ -24,6 +27,16 @@ export default {
     return {
       value: ""
     };
+  },
+  components: {
+    historyL
+  },
+  computed: {
+    ...mapState({
+      historyl(state) {
+        return state.historyl;
+      }
+    })
   }
 };
 </script>
