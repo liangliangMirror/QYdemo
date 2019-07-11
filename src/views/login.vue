b1e55236ed655916e26c08e6609071d1c60669be
MaxinFN <maxindeadpool@gemail.com>
2019-07-11 21:33:09 +0800

    login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,128 +1,23 @@
 <template>
-  <div>
-    <van-nav-bar title="穷游网" left-arrow @click-left.prevent="backto" />
-    <h2>注册/登录</h2>
-    <van-cell-group>
-      <div class="font-num">+86</div>
-      <van-field
-        style="display:inline-block;width:90%;"
-        v-model="username"
-        required
-        clearable
-        right-icon="question-o"
-        placeholder="请输入手机号码"
-      />
-    </van-cell-group>
-    <span class="login-tips">未注册手机号验证后自动创建</span>
-    <van-button round type="danger">下一步</van-button>
-    <div class="bot">
-      <div class="logpass">账号密码登录</div>
-      <div class="log-help">需要帮助？</div>
-    </div>
-  </div>
+  <mt-header title="multiple button">
+    <router-link to="/" slot="left">
+      <mt-button icon="back">back</mt-button>
+      <mt-button @click="handleClose">close</mt-button>
+    </router-link>
+    <mt-button icon="more" slot="right"></mt-button>
+  </mt-header>
 </template>
 <script>
 import Vue from "vue";
-import { NavBar } from "vant";
-import { Field } from "vant";
-import { CellGroup } from "vant";
-import { Button } from "vant";
-import { mapState, mapMutations } from "vuex";
-Vue.use(Field);
-Vue.use(NavBar);
-Vue.use(CellGroup);
-Vue.use(Button);
+import router from "vue-router";
+// import Mint from "mint-ui";
+import { Header, Button } from "mint-ui";
 
-export default {
-  data() {
-    return {
-      username: "",
-      password: ""
-    };
-  },
+// Vue.use(Mint);
+Vue.component(Header.name, Header);
+Vue.component(Button.name, Button);
 
-  created() {
-    console.log("thisC", this.$route);
-  },
-
-  methods: {
-    backto() {
-      // this.$router.push({});
-      console.log(this.history);
-      console.log(this.$router.go);
-      this.$router.go(-1);
-    }
-  }
-};
+export default {};
 </script>
-<style lang="scss" scoped>
-h2 {
-  color: #000;
-  padding-left: 1.7rem;
-  padding-top: 2.625rem;
-  font-size: 2.2rem;
-  line-height: 1.875rem;
-  padding-bottom: 3.125rem;
-}
-.van-field__control {
-  font-size: 0.75rem;
-  font-weight: bolder;
-}
-
-.van-nav-bar .van-icon {
-  color: #424242;
-}
-
-.van-cell-group {
-  padding: 5px;
-  display: flex;
-  align-content: center;
-}
-.van-button--hairline.van-button--round::after,
-.van-button--round {
-  position: relative;
-  top: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: rgba(21, 219, 145, 0.3);
-  border: none;
-  width: 20rem;
-  height: 2.5rem;
-  line-height: 2.5rem;
-}
-
-.font-num {
-  display: flex;
-  flex: 1;
-  // justify-content: center;
-  align-items: center;
-  font-size: 1rem;
-  font-weight: bolder;
-}
-
-.login-tips {
-  display: inline-block;
-  padding-top: 0.4rem;
-  color: rgba(0, 0, 0, 0.3);
-  font-size: 0.75rem;
-}
-
-.bot {
-  padding: 0 20px;
-  margin-top: 40px;
-  display: flex;
-  align-items: center;
-
-  justify-content: space-between;
-  font-size: 12px;
-  color: rgba(0, 0, 0, 0.6);
-}
-
-.logpass {
-  flex: 4;
-}
-
-.log-help {
-  flex: 1;
-}
+<style>
 </style>
